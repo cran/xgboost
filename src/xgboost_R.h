b629235a122beb0f42c445e9d9eb6c6a72b0474e
Tong He <hetong007@gmail.com>
2016-02-15 14:41:12 +0000

    version 0.4-3

--- src/xgboost_R.h
+++ src/xgboost_R.h
@@ -7,11 +7,11 @@
 #ifndef XGBOOST_WRAPPER_R_H_ // NOLINT(*)
 #define XGBOOST_WRAPPER_R_H_ // NOLINT(*)
 
-extern "C" {
+
 #include <Rinternals.h>
 #include <R_ext/Random.h>
 #include <Rmath.h>
-}
+
 
 extern "C" {
   /*!
